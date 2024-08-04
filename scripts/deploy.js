2193a7e8910c43c45b56d2825c102598cefddaa3
rakshithaks23 <rakshithaanr79@gmail.com>
2024-08-04 21:02:46 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,5 @@
 // We require the Hardhat Runtime Environment explicitly here. This is optional
+
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
